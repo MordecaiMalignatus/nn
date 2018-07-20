595ae198c8bb434b7d7f1abd9223e3be47cb03d6
Mordecai Malignatus <az4reus@gmail.com>
2018-07-20 11:29:09 +0200

    Feat(NN): basic functionality now works.

--- nn.go
+++ nn.go
@@ -1,27 +1,104 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"flag"
+  "strconv"
 	"fmt"
-  "io/ioutil"
-  "strings"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 	"time"
-  "flag"
 )
 
-const CONFIG_FILE = "~/.config/nn"
+const (
+  DEFAULT_EXTENSION = ".md"
+)
 
 func main() {
-  fmt.Println(parseFlags())
+  flags := parseFlags()
+  config := readConfigFile()
+
+  createNewNote(config, flags) 
 }
 
-func parseFlags() Opts {
-  filename := flag.String("f", getDate(), "Manually set the filename for a newly created card")
-  flag.Parse()
+// ==========================
+// Handling Flags
 
-  return Opts { *filename }
+func parseFlags() Flags {
+	filename := flag.String("f", getDate(), "Manually set the filename for a newly created card")
+	flag.Parse()
+
+	return Flags{*filename}
+}
+
+type Flags struct {
+	filepath string
+}
+
+// ==========================
+// Config File
+
+type Opts struct {
+	InboxPath string
+	Counter  int
+}
+
+func checkForConfig() {
+	config := getConfigPath()
+	_, err := os.Stat(config)
+	if os.IsNotExist(err) {
+    defaultInbox := getHomeDir() + "/newNotes/"
+		defaultOpts := Opts{InboxPath: defaultInbox, Counter: 0}
+
+		j, err := json.MarshalIndent(defaultOpts, "", "  ")
+		check(err)
+
+		err = ioutil.WriteFile(config, j, 0644)
+		check(err)
+	}
+}
+
+func readConfigFile() Opts {
+	checkForConfig()
+
+	data, err := ioutil.ReadFile(getConfigPath())
+	check(err)
+
+	var opts Opts
+	err = json.Unmarshal(data, &opts)
+	check(err)
+
+	return opts
+}
+
+func writeConfig(opts Opts) {
+  config := getConfigPath()
+  j, err := json.MarshalIndent(opts, "", "  ")
+  check(err)
+
+  err = ioutil.WriteFile(config, j, 0644)
+  check(err)
+}
+
+// ==========================
+// Creating a new note.
+
+func createNewNote(config Opts, flags Flags) {
+  fileName := createFileName(config)
+  prefabbedContent := defaultTextString()
+
+  err := ioutil.WriteFile(fileName, []byte(prefabbedContent), 0644)
+  check(err)
+
+  err = launchEditor(fileName)
+  check(err)
+
+  config.Counter += 1
+  writeConfig(config)
 }
 
 func getEditor() (string, error) {
@@ -34,48 +111,51 @@ func getEditor() (string, error) {
 	}
 }
 
-type Opts struct {
-  filename string
-}
-
-func readConfigFile() (map[string]string, error) {
-  checkForConfig()
-
-  data, err := ioutil.ReadFile(CONFIG_FILE)
+func launchEditor(filename string) error {
+  editorPath, err := getEditor()
   check(err)
 
-  configString := string(data)
-  ret := make(map[string]string)
-  
-  for _, line := range strings.Split(configString, "\n") {
-    separated := strings.Split(line, " = ")
-    ret[separated[0]] = separated[1]
-  }
-
-  return ret, nil
-}
-
-func launchEditor(editorPath string, filename []string) error {
-	cmdArgs := append([]string{"--"}, filename...)
+	cmdArgs := append([]string{"--"}, filename)
 	cmd := exec.Command(editorPath, cmdArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
-	fmt.Println(cmd.Run())
+	cmd.Run()
 
 	return nil
+}
+
+func createInboxDirIfNotExists(c Opts) {
+  os.MkdirAll(c.InboxPath, os.ModePerm)
+}
+
+func createFileName(c Opts) string {
+  return c.InboxPath + strconv.Itoa(c.Counter) + DEFAULT_EXTENSION 
 }
 
 func defaultTextString() string {
 	return fmt.Sprintf("# \n _(%s)_", getDate())
 }
 
+// ==========================
+// Util
+
 func getDate() string {
-  t := time.Now()
-  return t.String()[0:10]
+	t := time.Now()
+	return t.String()[0:10]
 }
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getHomeDir() string {
+	usr, _ := user.Current()
+	return usr.HomeDir
+}
+
+func getConfigPath() string {
+	home := getHomeDir()
+	return filepath.Join(home, ".config", "nn")
 }
